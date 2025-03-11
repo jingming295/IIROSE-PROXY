991273496f9c918b6a353dd8950768deeadc7690
Ming <48265334+jingming295@users.noreply.github.com>
2025-03-11 16:49:52 +0800

    INIT

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+class APP
+{
+    constructor()
+    {
+    }
+
+    async init()
+    {
+        if (typeof document === 'undefined')
+        {
+            console.log(
+                `%c [Ming's IIROSE-PROXY] - FAILED `,
+                `color: #FF5733; background: black; margin: 1em 0; padding: 5px 0; font-weight: 900`
+            );
+            return;
+        }
+
+        const mainFrame = document.getElementById('mainFrame') as HTMLIFrameElement | null;
+        const mainContainer = document.getElementById('mainContainer') as HTMLDivElement | null;
+        if (!mainContainer) return;
+
+        // 检查 mainFrame 是否存在并注入脚本
+        if (mainFrame && mainFrame.contentWindow && mainFrame.contentDocument)
+        {
+            this.injectScriptIntoIframe(mainFrame);
+        }
+
+        console.log(
+            `%c [Ming's IIROSE-PROXY] - LOADED `,
+            `color: #4CAF50; background: black; margin: 1em 0; padding: 5px 0; font-weight: 900`
+        );
+    }
+
+    injectScriptIntoIframe(iframe: HTMLIFrameElement)
+    {
+        // 获取 iframe 的文档对象
+        const iframeDocument = iframe.contentDocument || iframe.contentWindow?.document;
+        if (!iframeDocument)
+        {
+            console.log(
+                `%c [Ming's IIROSE-PROXY] - UNABLE TO GET IFRAME `,
+                `color: #FF5733; background: black; margin: 1em 0; padding: 5px 0; font-weight: 900`
+            );
+
+            return;
+        }
+
+        // 创建一个新的 script 标签
+        const script = iframeDocument.createElement('script');
+        script.type = 'module'; // 使用模块类型
+        script.textContent = `
+            (() => {
+                
+                const app = new (${APP.toString()})();
+                app.init();
+            })();
+        `;
+
+        // 将 script 标签插入到 iframe 的 body 中
+        iframeDocument.body.appendChild(script);
+    }
+}
+
+const app = new APP();
+app.init();
